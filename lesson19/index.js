7571751dd4cd045e033635dcff974d0ab639c2c3
SteamOCE <Obydakhalil212@gmail.com>
2025-06-24 21:13:21 +0800

    lesson19done

---
+++ lesson19/index.js
@@ -0,0 +1,17 @@
+// below is the code to print something in javascript
+console.log("Welcome obyda to the java script!")
+
+var obyda = 'Hi Obyda was here, I love football do not mess with me.'
+console.log(obyda)
+
+var ankush = 20
+var aus = true
+var uk = false
+
+var sydney = {gender_type: "Male", age: 15, name: "Obyda"}
+console.log(sydney.age)
+
+var a = 90
+var b = 15
+var sum = a + b
+console.log(sum)
