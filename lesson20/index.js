ad089f1c090d11a00cdaa91ddfae4ebd73997769
SteamOCE <Obydakhalil212@gmail.com>
2025-07-15 21:10:52 +0800

    lesson20

---
+++ lesson20/index.js
@@ -0,0 +1,12 @@
+function add(a,b,c,d){
+    var sum = a + b + c + d 
+    return sum
+}
+
+// console.log(sum)
+
+console.log(add(17,99,87328,93))
+console.log(add(0, 4,7,87))
+
+var a = add(7,4,3223,99)
+console.log(a)
