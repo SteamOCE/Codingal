e341b3762e190859415e1965052188a5e072ae10
SteamOCE <Obydakhalil212@gmail.com>
2025-07-23 21:10:58 +0800

    lesson22done

---
+++ lesson22/script.js
@@ -0,0 +1,10 @@
+var obyda = { australia: 20, west_Indies: 12, India: 30, USA: 40, dubai: 4.000 }
+
+var activity = {name: "Riya", age: 8 ,grade: 3,fav_color: 'Pink'}
+console.log(activity.fav_color)
+var name = "Riya"
+
+console.log(Math.random())
+console.log(Math.ceil(6.1111))
+console.log(Math.floor(5.573))
+console.log(Math.ceil(9.9999))
