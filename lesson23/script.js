d7a1c9802307bcb92e27930629bbee4edf985ee1
SteamOCE <Obydakhalil212@gmail.com>
2025-07-29 21:15:12 +0800

    lesson23done

---
+++ lesson23/script.js
@@ -0,0 +1,21 @@
+var a = [50, 80, 90, 20, 40, 104]
+var b = ['obyda', 'australia', 'perth', 'cupra']
+
+console.log(b[1])
+console.log(b[3])
+console.log(b[0])
+console.log(b[2])
+console.log(b[0])
+console.log(a[10])
+
+console.log(a.join("----"))
+console.log(a.push(99999))
+console.log(a.find((obyda => obyda > 0)))
+console.log(a)
+
+function sum(a, b, c) {
+    return sum(a, b, c) / 3
+}
+var result = average(10, 20, 30)
+
+console.log(result)
