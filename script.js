72c19265aba0d7ae5c89f7dcb089fcb5bd3ea65e
SteamOCE <Obydakhalil212@gmail.com>
2025-07-30 21:13:21 +0800

    lesson24complete

---
+++ script.js
@@ -0,0 +1,10 @@
+var a = document.getElementById("obyda")
+a.style.backroundColor = 'blue'
+
+
+var b = document.getElementsByClassName("Obyda class")[0]
+
+document.getElementsByTagName('a')[0].innerHTML = 'About us'
+document.getElementsByTagName('a')[1].innerHTML = 'Contact us'
+document.getElementsByTagName('a')[2].innerHTML = 'Homepage'
+document.getElementsByTagName('a')[3].innerHTML = 'Buy from us'
