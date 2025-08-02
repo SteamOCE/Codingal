5c2a0c72ef63381cda2039aa3102d8019a9134f5
SteamOCE <Obydakhalil212@gmail.com>
2025-08-02 21:13:02 +0800

    projectcomplete

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var a = document.getElementById("obyda")
-a.style.backroundColor = 'blue'
+a.style.backroundColor = 'red'
 
 
 var b = document.getElementsByClassName("Obyda class")[0]
 
-document.getElementsByTagName('a')[0].innerHTML = 'About us'
-document.getElementsByTagName('a')[1].innerHTML = 'Contact us'
-document.getElementsByTagName('a')[2].innerHTML = 'Homepage'
-document.getElementsByTagName('a')[3].innerHTML = 'Buy from us'
+document.getElementsByTagName('a')[0].innerHTML = 'cold'
+document.getElementsByTagName('a')[1].innerHTML = 'hot'
+document.getElementsByTagName('a')[2].innerHTML = 'rain'
+document.getElementsByTagName('a')[3].innerHTML = 'foggy'
