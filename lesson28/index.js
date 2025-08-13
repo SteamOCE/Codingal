65194c8e827f352d7f433fb8ca99572342e090cc
SteamOCE <Obydakhalil212@gmail.com>
2025-08-13 21:14:59 +0800

    lesson28

---
+++ lesson28/index.js
@@ -0,0 +1,35 @@
+var a = 'obyda'
+var b = 59
+var c = true
+var d = [9, 10, 34, 76, 39]
+var e = { name: 'obyda', country: 'australia' }
+
+console.log(typeof (a))
+console.log(typeof (b))
+console.log(typeof (c))
+console.log(typeof (d))
+console.log(typeof (e))
+
+console.log(typeof (String(b)))
+console.log(Number(a))
+console.log(Number(d))
+
+
+var w = 'efwrgetwhreyjtrk7yt8l urfweidqpj  gfdfghjklllear wrgwqgog wggq4g4g 4g5h6j 7k8l9 5 hh6j57k8 h6e5j7k6l idrwebntworvsdcpeovrne frwgethrynjukil th4yj5uk687l98 frgthy5j76k8 fgew5rhe6jr76k8l thryjtemruk678il98 obyda r4t5y6u7i8o76 rgthyjuki'
+
+var searchfor = 'obyda'
+
+var x = w.search('obyda')
+
+console.log(x)
+
+try{
+    // riskier code which might give an error
+    const y = 'awefregtrhyju'
+    y = 'eqfwrgtwhyj'
+}catch(eer){
+    //handing the error
+    console.log(err)
+}
+
+console.log('important code running')
