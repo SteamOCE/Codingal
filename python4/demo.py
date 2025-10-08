97203a0cf08d9f7f483e436279e3c32a184f381d
SteamOCE <Obydakhalil212@gmail.com>
2025-10-08 21:14:01 +0800

    submitpython4

---
+++ python4/demo.py
@@ -0,0 +1,32 @@
+# print("wash your hands 1st time")
+# print("hahahahahahaha")
+
+
+# print("wash your hands 2nd time")
+# print("hahhahahahahahahahahaha")
+
+# print("wash your hands 3rd time")
+# print("hahahahahahahahahahahahhahaha")
+
+
+def washYourHand(time):
+    print(f"wash your hand {time} th time")
+
+
+washYourHand(1)
+
+washYourHand(2)
+
+
+# print("Please taka a bath")
+# print("Wash yourself")
+
+def obydaFunction(name, age, phone):
+    print(f"Hi my name is {name} and my age is {age} and my phone number is {phone}")
+    print("hip hip hurray")
+
+
+obydaFunction('Ronaldo', 38, '8332323232')
+obydaFunction('Spiderman', 45, '2834y833dfsd')
+obydaFunction('Obyda', 16, '34534534')
+
